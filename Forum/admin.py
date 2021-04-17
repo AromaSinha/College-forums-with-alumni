bbc9445d7cbc0049d6f4e72f0477c66a3ab59be6
AromaSinha <73636017+AromaSinha@users.noreply.github.com>
2021-04-17 20:51:05 +0530

    Add files via upload

---
+++ Forum/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from.models import*
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Suggest)
+admin.site.register(forum)
+admin.site.register(Discussion)
